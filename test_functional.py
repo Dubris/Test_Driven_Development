f36993fee3dfe4be2f1efadeeacb2510eb1f7e04
Alexandre Le Bris <alexandre.le-bris@gfi.fr>
2018-10-16 20:28:01 +0200

    Model for list Items and associated migrations

--- test_functional.py
+++ test_functional.py
@@ -11,6 +11,10 @@ def browser():
     yield browser
     browser.quit()
 
+def check_for_row_in_list_table(browser, row_text):
+    table = browser.find_element_by_id('id_list_table')
+    rows = table.find_elements_by_tag_name('tr')
+    assert row_text in [row.text for row in rows]
 
 def test_can_start_a_list_and_retrieve_it_later(browser):
     # Edith has heard about a cool new online to-do app. She goes
@@ -34,16 +38,19 @@ def test_can_start_a_list_and_retrieve_it_later(browser):
     # "1: Buy peacock feathers" as an item in a to-do list
     inputbox.send_keys(Keys.ENTER)
     time.sleep(0.1)
-
-    table = browser.find_element_by_id('id_list_table')
-    rows = table.find_elements_by_tag_name('tr')
-    assert '1: Buy peacock feathers' in [row.text for row in rows]
-    pytest.fail('Finish the test')
+    check_for_row_in_list_table(browser, '1: Buy peacock feathers')
 
     # There is still a text box inviting her to add another item. She
     # enters "Use peacock feathers to make a fly" (Edith is very methodical)
-
+    inputbox = browser.find_element_by_id('id_new_item')
+    inputbox.send_keys('Use peacock feathers to make a fly')
+    inputbox.send_keys(Keys.ENTER)
+    time.sleep(0.1)
+    
     # The page updates again, and now shows both items on her list
+    check_for_row_in_list_table(browser, '1: Buy peacock feathers')
+    check_for_row_in_list_table(browser, '2: Use peacock feathers to make a fly')
+    pytest.fail('Finish the test')
 
     # Edith wonders whether the site will remember her list. Then
     # she sees that the site has generated a unique URL for her --
