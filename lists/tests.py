a06134bd43d201fe3a7c5bb249cb4eaca7d5961d
Alexandre Le Bris <alexandre.le-bris@gfi.fr>
2018-10-19 21:19:30 +0200

    new URL + view for adding to existing lists. FT passes. Remains 2 Unit tests to check

--- lists/tests.py
+++ lists/tests.py
@@ -3,10 +3,11 @@ from django.core.urlresolvers import resolve
 from django.http import HttpRequest
 from django.template.loader import render_to_string
 
-from lists.models import Item
+from lists.models import Item, List
 from lists.views import home_page
 
 import pytest
+import re
 
 class TestHomePage():
 
@@ -29,32 +30,53 @@ class TestListView():
 
     simpletestcase = SimpleTestCase()
 
-    def test_home_page_displays_all_list_items(self, client):
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
+    def test_displays_only_items_for_that_list(self, client):
+        correct_list = List.objects.create()
+        Item.objects.create(text='itemey 1', list=correct_list)
+        Item.objects.create(text='itemey 2', list=correct_list)
+        other_list = List.objects.create()
+        Item.objects.create(text='other item list 1', list=other_list)
+        Item.objects.create(text='other item list 2', list=other_list)
         
-        response = client.get('/lists/the-only-list-in-the-world/')
+        response = client.get('/lists/%d/' % (correct_list.id))
         
         assert self.simpletestcase.assertContains (response, "itemey 1") is None
         assert self.simpletestcase.assertContains (response, "itemey 2") is None
+        assert self.simpletestcase.assertNotContains (response, "other item list 1") is None
+        assert self.simpletestcase.assertNotContains (response, "other item list 2") is None
 
 
     def test_uses_list_template(self, client):
-        response = client.get('/lists/the-only-list-in-the-world/')
+        list_ = List.objects.create()
+        response = client.get('/lists/%d/' % (list_.id,))
         assert self.simpletestcase.assertTemplateUsed(response, 'list.html') is None
+        
+    def test_passes_correct_list_to_template(self, client):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+        response = client.get('/lists/%d/' % (correct_list.id,))
+        response.context['list'] == correct_list
 
 
-class TestItemModel():
+class TestListAndItemModel():
 
 
     def test_saving_and_retrieving_items(self):
+            list_ = List()
+            list_.save()
+            
             first_item = Item()
             first_item.text = 'The first (ever) list item'
+            first_item.list = list_
             first_item.save()
 
             second_item = Item()
             second_item.text = 'Item the second'
+            second_item.list = list_
             second_item.save()
+            
+            saved_list = List.objects.first()
+            assert saved_list == list_
             
             saved_items = Item.objects.all()
             assert saved_items.count() == 2
@@ -62,7 +84,9 @@ class TestItemModel():
             first_saved_item = saved_items[0]
             second_saved_item = saved_items[1]
             assert first_saved_item.text == 'The first (ever) list item'
+            assert first_saved_item.list == list_
             assert second_saved_item.text == 'Item the second'
+            assert second_saved_item.list == list_
             
 
 class TestNewList():
@@ -84,8 +108,46 @@ class TestNewList():
         response = client.post('/lists/new',
         data={'item_text': "A new list item"}
         )
+        new_list = List.objects.first()
 
         assert response.status_code == 302
-        assert self.simpletestcase.assertRedirects(
-        response, '/lists/the-only-list-in-the-world/'
-        ) is None
+        self.simpletestcase.assertRedirects(
+        response, '/lists/%d/' % (new_list.id)
+        )
+
+
+class TestNewItem():
+
+    simpletestcase = SimpleTestCase()
+
+    def test_can_save_a_POST_request_to_an_existing_list(self, client):
+        #other_list = List.objects.create()
+        correct_list = List.objects.create()
+        
+        url_target = 'lists/%d/add_item' % (correct_list.id,)
+        print(url_target)
+        print(re.match(r'^lists/(\d+)/add_item$', url_target))
+        
+        
+        client.post(url_target,
+        data = {'item_text': 'A new item for an existing list'}
+        )
+
+        assert Item.objects.count() == 1
+        new_item = Item.objects.first()
+        assert new_item.text == 'A new item for an existing list'
+        assert new_item.list == correct_list
+
+
+    def test_redirects_to_list_view(self, client):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+        
+        response = client.post('lists/%d/add_item' % (correct_list.id,),
+        data = {'item_text': 'A new item for an existing list'}
+        )
+        
+        assert response.status_code == 302
+        self.simpletestcase.assertRedirects(response,
+        'lists/%d/' % (correct_list.id, )
+        )
--- superlists/urls.py
+++ superlists/urls.py
@@ -15,11 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.conf.urls import patterns, include, url
-from lists.views import home_page
+import lists
+import re
 
 
-urlpatterns = patterns('',
-    url(r'^$', 'lists.views.home_page', name='home'),
-    url(r'^lists/the-only-list-in-the-world/$', 'lists.views.view_list', name='view_list'),
-    url(r'^lists/new', 'lists.views.new_list', name='new_list'),
-)
+urlpatterns= [
+    url(r'^$', lists.views.home_page, name='home'),
+    url(r'^lists/(\d+)/$', lists.views.view_list, name='view_list'),
+    url(r'^lists/(\d+)/add_item$', lists.views.add_item, name='add_item'),
+    url(r'^lists/new$', lists.views.new_list, name='new_list'),
+]
