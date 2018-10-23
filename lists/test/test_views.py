1b8eff8858df4b6011724d2f090a7f45f7084cda
Alexandre Le Bris <alexandre.le-bris@gfi.fr>
2018-10-23 14:57:48 +0200

    Split out unit tests into two files

--- lists/test/test_views.py
+++ lists/test/test_views.py
@@ -7,7 +7,7 @@ from lists.models import Item, List
 from lists.views import home_page
 
 import pytest
-import re
+
 
 class TestHomePage():
 
@@ -56,37 +56,6 @@ class TestListView(TestCase):
         response = self.client.get('/lists/%d/' % (correct_list.id,))
         assert response.context['list'] == correct_list
 
-
-class TestListAndItemModel(TestCase):
-
-
-    def test_saving_and_retrieving_items(self):
-            list_ = List()
-            list_.save()
-            
-            first_item = Item()
-            first_item.text = 'The first (ever) list item'
-            first_item.list = list_
-            first_item.save()
-
-            second_item = Item()
-            second_item.text = 'Item the second'
-            second_item.list = list_
-            second_item.save()
-            
-            saved_list = List.objects.first()
-            assert saved_list == list_
-            
-            saved_items = Item.objects.all()
-            assert saved_items.count() == 2
-            
-            first_saved_item = saved_items[0]
-            second_saved_item = saved_items[1]
-            assert first_saved_item.text == 'The first (ever) list item'
-            assert first_saved_item.list == list_
-            assert second_saved_item.text == 'Item the second'
-            assert second_saved_item.list == list_
-            
 
 class TestNewList(TestCase):
 
---
+++ test_functional/test_list_item_validation.py
@@ -0,0 +1,33 @@
+from base import TestFunctional
+
+
+class TestItemValidation(TestFunctional):
+    
+    
+    def test_cannot_add_empty_list_items(self):
+        # Edith goes to the home page and accidentally tries
+        # to submit an empty list item. She hits Enter on the empty list
+        self.browser.get(self.server_url)
+        self.browser.find_element_by_id('id_new_item').send_keys('\n')
+        
+        # The home page refreshes, and there is an error saying
+        # that list item cannot be blank
+        error = self.browser.find_element_by_css_selector('.has-error')
+        assert error.text == "You can't have an empty list item"
+        
+        # She tries again with some text for the item, and it works
+        self.browser.find_element_by_id('id_new_item').send_keys('Buy milk\n')
+        self.check_for_row_in_list_table('1: Buy milk')
+        
+        # Perversely, she now tries to submit a second blank list item
+        self.browser.find_element_by_id('id_new_item').send_keys('\n')
+        
+        # She receives a similar warning on the list page
+        self.check_for_row_in_list_table('1: Buy milk')
+        error = self.browser.find_element_by_css_selector('.has-error')
+        assert error.text == "You can't have an empty list item"
+        
+        # And she can correct it by filling some text in
+        self.browser.find_element_by_id('id_new_item').send_keys('Make tea\n')
+        self.check_for_row_in_list_table('1: Buy milk')
+        self.check_for_row_in_list_table('2: Make tea')
