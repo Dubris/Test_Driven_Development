b56314c0f7b148b50f8242c385ef12dd7c42a3d5
Alexandre Le Bris <alexandre.le-bris@gfi.fr>
2018-10-24 15:39:50 +0200

    use form in all views, back to working state

--- lists/test/test_views.py
+++ lists/test/test_views.py
@@ -6,25 +6,23 @@ from django.utils.html import escape
 
 from lists.models import Item, List
 from lists.views import home_page
+from lists.forms import ItemForm, EMPTY_LIST_ERROR
 
 import pytest
 
 
-class TestHomePage():
+class TestHomePage(TestCase):
+    maxDiff = None
 
 
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/')
-        assert found.func == home_page
-
+    def test_home_page_renders_home_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
         
-    def test_home_page_returns_correct_html(self):
-        request = HttpRequest()
-
-        response = home_page(request)
-        expected_html = render_to_string('home.html')
-
-        assert expected_html == response.content.decode()
+        
+    def test_home_page_uses_item_form(self):
+        response = self.client.get('/')
+        self.assertIsInstance(response.context['form'], ItemForm)
 
 
 class TestListView(TestCase):
@@ -64,7 +62,7 @@ class TestListView(TestCase):
         correct_list = List.objects.create()
         
         self.client.post('/lists/%d/' % (correct_list.id,),
-        data = {'item_text': 'A new item for an existing list'}
+        data = {'text': 'A new item for an existing list'}
         )
 
         assert Item.objects.count() == 1
@@ -78,18 +76,42 @@ class TestListView(TestCase):
         correct_list = List.objects.create()
         
         response = self.client.post('/lists/%d/' % (correct_list.id,),
-        data = {'item_text': 'A new item for an existing list'}
+        data = {'text': 'A new item for an existing list'}
         )
         self.assertRedirects(response, 'lists/%d/' % (correct_list.id,))
         
         
-    def test_validation_errors_end_up_on_lists_page(self):
-        list_=List.objects.create()
-        response = self.client.post('/lists/%d/' % (list_.id,), data={'item_text': ''})
+    def post_invalid_input(self):
+        list_ = List.objects.create()
+        return self.client.post('/lists/%d/' % (list_.id,), data={'text': ''})
+        
+        
+    def test_for_invalid_input_nothing_saved_to_db(self):
+        self.post_invalid_input()
+        assert Item.objects.count() == 0
+        
+        
+    def test_for_invalid_input_renders_list_template(self):
+        response = self.post_invalid_input()
         assert response.status_code == 200
         self.assertTemplateUsed(response, 'list.html')
-        expected_error = escape("You can't have an empty list item")
-        self.assertContains(response, expected_error)
+        
+        
+    def test_for_invalid_input_passes_form_to_template(self):
+        response = self.post_invalid_input()
+        self.assertIsInstance(response.context['form'], ItemForm)
+        
+        
+    def test_for_invalid_input_shows_error_on_page(self):
+        response = self.post_invalid_input()
+        self.assertContains(response, escape(EMPTY_LIST_ERROR))
+
+        
+    def test_displays_item_form(self):
+        list_ = List.objects.create()
+        response = self.client.get('/lists/%d/' % (list_.id,))
+        self.assertIsInstance(response.context['form'], ItemForm)
+        self.assertContains(response, 'name="text"')
 
 
 class TestNewList(TestCase):
@@ -97,7 +119,7 @@ class TestNewList(TestCase):
 
     def test_saving_a_POST_request(self):
         self.client.post('/lists/new',
-        data={'item_text': 'A new list item'}
+        data={'text': 'A new list item'}
         )
         
         assert Item.objects.count() == 1
@@ -107,23 +129,32 @@ class TestNewList(TestCase):
 
     def test_redirects_after_POST(self):
         response = self.client.post('/lists/new',
-        data={'item_text': "A new list item"}
+        data={'text': "A new list item"}
         )
         new_list = List.objects.first()
 
         assert response.status_code == 302
         self.assertRedirects(response, '/lists/%d/' % (new_list.id))
         
-    
-    def test_validation_errors_are_sent_back_to_home_page_template(self):
-        response = self.client.post('/lists/new', data={'item_text': ''})
+        
+    def test_for_invalid_input_renders_home_template(self):
+        response = self.client.post('/lists/new', data={'text': ''})
         assert response.status_code == 200
         self.assertTemplateUsed(response, 'home.html')
-        expected_error = escape("You can't have an empty list item")
-        self.assertContains(response, expected_error)
+        
+        
+    def test_validation_errors_are_shown_on_home_page(self):
+        response = self.client.post('/lists/new', data={'text': ''})
+        self.assertContains(response, escape(EMPTY_LIST_ERROR))
+        
+        
+    def test_for_invalid_input_passes_form_to_template(self):
+        response = self.client.post('/lists/new', data={'text': ''})
+        self.assertIsInstance(response.context['form'], ItemForm)
+        
         
     def test_invalid_list_items_arent_saved(self):
-        response = self.client.post('/lists/new', data={'item_text': ''})
+        response = self.client.post('/lists/new', data={'text': ''})
         assert List.objects.count() == 0
         assert Item.objects.count() == 0
         
--- test_functional/test_simple_list_creation.py
+++ test_functional/test_simple_list_creation.py
@@ -19,7 +19,7 @@ class TestNewVisitor(TestFunctional):
         assert 'To-Do' in header_text
 
         # She is invited to enter a to-do item straight away
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.get_item_input_box()
         assert inputbox.get_attribute('placeholder') == 'Enter a to-do item'
 
         # She types "Buy peacock feathers" into a text box (Edith's hobby
@@ -36,7 +36,7 @@ class TestNewVisitor(TestFunctional):
 
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very methodical)
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.get_item_input_box()
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(0.1)
@@ -60,7 +60,7 @@ class TestNewVisitor(TestFunctional):
         
         # Francis starts a new list by entering a new item. He is less
         # interesting than Edith...
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.get_item_input_box()
         inputbox.send_keys('Buy milk')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(0.1)
